79e056c5f3b0ef6c8ce213804d2b85c49f514f98
emyede <96616310+yanz17@users.noreply.github.com>
2025-08-05 17:27:17 +0700

    update techstack animation

--- src/app/components/TechStack.jsx
+++ src/app/components/TechStack.jsx
@@ -1,12 +1,23 @@
 "use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 
 export default function TechStack({ stacks }) {
+  const [order, setOrder] = useState(stacks);
+
+  // Shuffle posisi tiap 2 detik
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setOrder((prev) => shuffle(prev));
+    }, 2000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <section className="py-20">
       <motion.h2
-        className="text-5xl font-bold text-center text-[#76ABAE] mb-25"
+        className="text-5xl font-bold text-center text-[#76ABAE] mb-10"
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
         transition={{ duration: 0.6 }}
@@ -14,19 +25,37 @@ export default function TechStack({ stacks }) {
         Tech Stack
       </motion.h2>
 
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-25 my-25 place-items-center">
-        {stacks.map((stack, i) => (
-          <motion.div
+      <motion.ul
+        layout
+        className="flex flex-wrap justify-center gap-10 mt-25 mb-15"
+      >
+        {order.map((stack, i) => (
+          <motion.li
             key={stack.name}
-            initial={{ opacity: 0, y: 30 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            viewport={{ once: true }}
-            transition={{ delay: i * 0.1 }}
+            layout
+            transition={{
+              type: "spring",
+              damping: 20,
+              stiffness: 300,
+            }}
+            className="flex justify-center items-center w-24 h-24 rounded-xl bg-transparent"
           >
-            <Image src={stack.logo} alt={stack.name} width={75} height={75} />
-          </motion.div>
+            <Image
+              src={stack.logo}
+              alt={stack.name}
+              width={75}
+              height={75}
+            />
+          </motion.li>
         ))}
-      </div>
+      </motion.ul>
     </section>
   );
+}
+
+/**
+ * Utils: Random shuffle array
+ */
+function shuffle(array) {
+  return [...array].sort(() => Math.random() - 0.5);
 }
